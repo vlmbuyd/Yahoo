39981d4bb9317c10e50278fc78797244453d8043
Yean <kjan0929@naver.com>
2024-11-28 14:38:25 +0900

    style: 배너 광고 추가

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,15 @@
 import { CategoryNewsList } from "@/components/category-news-feed/CategoryNewsList";
 import { Header } from "@/components/common/Header";
-import bannerAds_1 from "/banner-ads/banner-ads-1.webp";
-import bannerAds_2 from "/banner-ads/banner-ads-2.webp";
 import { SideBar } from "@/components/common/SideBar";
 import { RealTimeSearchTrends } from "@/components/common/RealTimeSearchTrends";
 import { RecoNewsFeedList } from "@/components/reco-news-feed/RecoNewsFeedList";
 import { AlertDashboard } from "@/components/common/AlertDashboard";
+import bannerAds_1 from "/banner-ads/banner-ads-1.webp";
+import bannerAds_2 from "/banner-ads/banner-ads-2.webp";
+import bannerAds_3 from "/banner-ads/banner-ads-3.webp";
+import leftArrowIcon from "@/assets/common/left_arrow.svg";
+import rightArrowIcon from "@/assets/common/right_arrow.svg";
+import bannerPageNum from "@/assets/icons/banner-ads-page-num.svg";
 
 export const MainPage = () => {
   return (
@@ -32,13 +36,41 @@ export const MainPage = () => {
         <section className="flex flex-col flex-1 gap-[24px] min-w-[290px] max-w-[384px]">
           <RealTimeSearchTrends />
           <div className="w-full max-h-[184px]">
-            <img
-              src={bannerAds_2}
-              alt="banner-advertising"
-              className="block w-full h-full"
-            />
+            <img src={bannerAds_2} alt="banner-advertising" />
           </div>
           <AlertDashboard />
+          <div className="relative w-full h-[348px]">
+            <img src={bannerAds_3} alt="banner-advertising" className="w-[" />
+
+            <button
+              className="flex justify-center items-center absolute top-[50%] -translate-y-1/2 left-0 w-[36px] h-[52px]"
+              style={{ backgroundColor: "rgba(0,0,0,0.2)" }}
+            >
+              <img src={leftArrowIcon} alt="left-arrow-icon" />
+            </button>
+            <button
+              className="flex justify-center items-center absolute top-[50%] -translate-y-1/2 right-0 w-[36px] h-[52px]"
+              style={{ backgroundColor: "rgba(0,0,0,0.2)" }}
+            >
+              <img src={rightArrowIcon} alt="right-arrow-icon" />
+            </button>
+
+            <div className="flex flex-col items-start absolute bottom-[24px] left-[16px]">
+              <span className="mb-[1px] text-[10px] font-normal text-[#ffffff]">
+                ZIGZAG
+              </span>
+              <strong className="mb-[6px] text-[19px] font-normal text-[#ffffff]">
+                あふれ出た割引 <br />
+                今シーズンはこれで！
+              </strong>
+              <p className="text-[11px] font-normal text-[#ffffff]">
+                オンライン単独割引~20%クーポン
+              </p>
+            </div>
+            <div className="absolute bottom-[12px] right-[16px]">
+              <img src={bannerPageNum} alt="banner-ads-page-num" />
+            </div>
+          </div>
         </section>
       </div>
     </div>

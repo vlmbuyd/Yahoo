5174d5faa3adf6f206c193a11a04b4d52baeb83d
Yean <kjan0929@naver.com>
2024-12-03 01:08:58 +0900

    feat: 카테고리별 기사 메뉴 로직 구현 및 실시간 검색어 링크 연동

--- src/components/category-news-feed/CategoryNewsList.tsx
+++ src/components/category-news-feed/CategoryNewsList.tsx
@@ -1,13 +1,25 @@
-import feedMenu from "@/mock/category-news-menu.json";
 import feedData from "@/mock/category-news-feed.json";
 import { CategoryFeedType } from "@/types/feed";
 import { CategoryNewsFeed } from "./CategoryNewsFeed";
 import arrowLeft from "@/assets/category-news-feed/arrow_left.svg";
 import arrowRight from "@/assets/category-news-feed/arrow_right.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const CategoryNewsList = () => {
-  const menus: string[] = feedMenu;
+  const menus: string[] = [
+    "主要",
+    "経済",
+    "エンタメ",
+    "スポーツ",
+    "国内",
+    "国際",
+    "IT",
+    "科学",
+    "地域",
+  ];
   const data = feedData as CategoryFeedType[];
+  const [selectedCategory, setSelectedCategory] = useState<string>("主要");
 
   return (
     <section className="w-full h-[320px] pt-[20px] rounded-[10px] bg-[white] shadow-[1px_2px_10px_rgba(0,0,0,0.05)]">
@@ -16,7 +28,14 @@ export const CategoryNewsList = () => {
           {menus.map((menu, idx) => (
             <li
               key={`category-news-menu-${idx}`}
-              className="flex justify-center items-center w-[81px] h-[26px] border-r border-solid border-[#d7d7d7] last:border-none cursor-pointer"
+              className={`flex justify-center items-center w-[81px] h-[26px] border-r border-solid border-[#d7d7d7] text-[17px] last:border-none cursor-pointer
+                ${
+                  selectedCategory === menu
+                    ? "font-semibold text-[#4D45D6]"
+                    : "font-medium text-[#999999]"
+                }
+                `}
+              onClick={() => setSelectedCategory(menu)}
             >
               {menu}
             </li>
@@ -26,7 +45,9 @@ export const CategoryNewsList = () => {
 
       <div className="flex justify-center flex-wrap gap-x-[20px] gap-y-[16px] mb-[4px]">
         {data.map((feed, idx) => (
-          <CategoryNewsFeed key={`category-news-feed-${idx}`} {...feed} />
+          <Link to={feed.url}>
+            <CategoryNewsFeed key={`category-news-feed-${idx}`} {...feed} />
+          </Link>
         ))}
       </div>
 
--- src/components/common/RealTimeSearchTrends.tsx
+++ src/components/common/RealTimeSearchTrends.tsx
@@ -1,9 +1,12 @@
 import trendData from "@/mock/realtime-search-trends.json";
 import upRankArrow from "@/assets/icons/up-rank-arrow.svg";
 import downRankArrow from "@/assets/icons/down-rank-arrow.svg";
+import { Link } from "react-router-dom";
 
 export const RealTimeSearchTrends = () => {
-  const data: { content: string; trendingUp: boolean }[] = trendData;
+  const data: { content: string; trendingUp: boolean; url: string }[] =
+    trendData;
+
   return (
     <section className="min-w-[290px] max-w-[384px] min-h-[320px] max-h-[346px] pt-[20px] pl-[20px] pr-[14px] pb-[12px] rounded-[12px] bg-[#ffffff] shadow-[1px_2px_10px_rgba(0,0,0,0.05)]">
       <h3 className="text-[19px] mb-[24px] font-bold text-[#222222]">
@@ -11,17 +14,19 @@ export const RealTimeSearchTrends = () => {
       </h3>
       <ul className="flex flex-col gap-[18px] mb-[12px]">
         {data.map((item, idx) => (
-          <li key={`trend-keyword-${idx}`} className="flex items-center ">
-            <strong className="flex justify-center items-center w-[28px] h-[28px] rounded-[8px] mr-[2px] text-[14px] font-semibold text-[#333333] bg-[#f1f3f5]">
-              {idx + 1}
-            </strong>
-            <img
-              src={item.trendingUp ? upRankArrow : downRankArrow}
-              alt="trend-rank-icon"
-              className="mr-[4px]"
-            />
-            <h4>{item.content}</h4>
-          </li>
+          <Link to={item.url}>
+            <li key={`trend-keyword-${idx}`} className="flex items-center ">
+              <strong className="flex justify-center items-center w-[28px] h-[28px] rounded-[8px] mr-[2px] text-[14px] font-semibold text-[#333333] bg-[#f1f3f5]">
+                {idx + 1}
+              </strong>
+              <img
+                src={item.trendingUp ? upRankArrow : downRankArrow}
+                alt="trend-rank-icon"
+                className="mr-[4px]"
+              />
+              <h4>{item.content}</h4>
+            </li>
+          </Link>
         ))}
       </ul>
 
